8b16a9af7f499484fd3fc24077caa648e225c44c
Anthonylilo <ammen0203@gmail.com>
2024-07-12 20:43:29 -0400

    First config for inertia.js

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <AboutMe />
+            <Experience />
+            <Projects />
+            <Education />
+            <HireMe />
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Portfolio</title>
+</head>
+<body>
+    @inertia
+    <script src="{{ mix('js/app.jsx') }}" defer></script>
+</body>
+</html>

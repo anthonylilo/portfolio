6b351fa3d45fb1aad3ca295460f7be8c3e7ecbd4
Anthonylilo <ammen0203@gmail.com>
2024-07-31 18:20:36 -0400

    Sidebar

--- resources/js/components/admin/sidebar/sidebar.jsx
+++ resources/js/components/admin/sidebar/sidebar.jsx
@@ -7,26 +7,28 @@ function SideBar() {
                 <h1>Anthony Lilo</h1>
             </div>
             <hr />
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">About Me</a>
-                </li>
-                <li>
-                    <a href="#">Experience</a>
-                </li>
-                <li>
-                    <a href="#">Projects</a>
-                </li>
-                <li>
-                    <a href="#">Education</a>
-                </li>
-                <li>
-                    <a href="#">Hire Me</a>
-                </li>
-            </ul>
+            <div className="sidebar-links">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#">Experience</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#">Education</a>
+                    </li>
+                    <li>
+                        <a href="#">Hire Me</a>
+                    </li>
+                </ul>
+            </div>
         </SideBarStyles>
     );
 }
--- resources/js/components/admin/sidebar/styleSideBar.js
+++ resources/js/components/admin/sidebar/styleSideBar.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const SideBarStyles = styled.div`
     width: 300px;
     height: 100%;
-    padding: 2%;
     position: fixed;
     z-index: 1;
     top: 0;
@@ -11,10 +10,43 @@ export const SideBarStyles = styled.div`
     background-color: #111;
     color: #fff;
 
-    hr{
+    hr {
         width: 100%;
         height: 1px;
         background-color: #fff;
         border: none;
     }
-`
+
+    .sidebar-links {
+        font-size: 20px;
+        color: #fff;
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        ul {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            flex-wrap: wrap;
+        }
+        li {
+            width: 90%;
+            height: 50px;
+            list-style: none;
+            background-color: pink;
+            margin-bottom: 3%;
+            border-radius: 5rem;
+            text-align: left;
+            padding: 2%;
+            a {
+                color: #fff;
+                cursor: pointer;
+                text-decoration: none;
+            }
+        }
+    }
+`;
